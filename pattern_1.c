96d22c5954dd54714aa26b1cead58b9678c5d7ea
MOHIT ANAND <mohitanand08@gmail.com>
2019-02-22 16:27:13 +0530

    Update pattern_1.c

--- pattern_1.c
+++ pattern_1.c
@@ -6,20 +6,20 @@ int main()
 
     /* Input number of rows from user */
     printf("Enter number of rows: ");
-    scanf("%d", &N);
+    scanf("%d", N);
 
     /* Iterate over each row */
     for(i=1; i<=N; i++)
     {
         /* Iterate over each column */
-        for(j=1; j<=N; j++)
+        for(j=1; j<=N j++)
         {
-            if(i==1 || i==N || j==1 || j=N)
+            if(i=1 || i=N || j=1 || j=N)
             {
                 /* Print star for 1st, Nth row and column */
                 printf("*");
             }
-            else
+            else if
             {
                 printf(" ");
             }
